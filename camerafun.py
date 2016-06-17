0ba32b5ca64279becd3a74b2db84b8e26df2cb33
Aleksey <amatiych@me.com>
2016-06-17 12:18:29 -0400

    first commit

---
+++ camerafun.py
@@ -0,0 +1,98 @@
+"""
+    camera functions
+"""
+import picamera
+import io
+from datetime import datetime
+import time
+from fractions import Fraction
+import os
+
+
+def capture_ex(fun):
+        def wrapper(*args, **kwargs):
+                try:
+                        return fun(*args,**kwargs)
+                except Exception as ex:
+                        print(ex)
+        return wrapper
+
+def sorted_ls(path):
+        mtime = lambda f: os.stat(os.path.join(path, f)).st_mtime
+        return list(sorted(os.listdir(path), key=mtime))
+
+def getfiles(path):
+        files = [file for file in sorted_ls(path) if "h264" in file]
+        return files
+
+def get_file_name(files_to_keep,home):
+        while True:
+                files = getfiles(home)
+                n = len(files)
+                if n == files_to_keep:
+                        oldest = files[0]
+                        print("removing " + oldest)
+                        os.remove(os.path.join(home,oldest))
+                filename = datetime.now().strftime("%Y%m%d_%H%M%s.h264")
+                yield os.path.join(home,filename)
+
+def dashcam(length, files):
+        folder = "/home/pi/Documents/python/homecamera"
+        for filename in get_file_name(files,home=folder):
+
+                with picamera.PiCamera() as cam:
+                        try:
+                                print("recording " + filename)
+                                cam.start_recording(filename)
+                                cam.wait_recording(length)
+                                cam.stop_recording()
+                        except Exception as ex:
+                                print(ex)
+
+
+def gen_videos(name,mult):
+        stream = io.BytesIO()
+        w = int(640 * mult)
+        h = int(480 * mult)
+        with picamera.PiCamera(resolution=(w,h)) as camera:
+                camera.annotate_text_size = int(20*mult)
+                camera.vflip = False
+                #camera.frame_rate = 10
+                for _ in camera.capture_continuous(stream,format='jpeg',use_video_port=True):
+                        time.sleep(0.5)
+                        camera.annotate_text = name + ' ' + time.ctime()
+                        frame = stream.getvalue()
+                        stream.truncate()
+                        stream.seek(0)
+                        yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+@capture_ex
+def capture_picture(name, scale):
+        print(str.format("capture picture  {0}:{1}", name, scale))
+        ext = "camera:"
+        w = int(640 * scale)
+        h = int(480 * scale)
+        print(str.format("capture picture  {0}:{1}", w,h))
+        stream = io.BytesIO()
+        stream.seek(0)
+        with picamera.PiCamera(resolution=(w,h)) as camera:
+
+                camera.start_preview()
+                #camera.exposure_mode  ='night'
+                camera.annotate_text = name +' ' + time.ctime()
+                camera.vflip = False
+                time.sleep(1)
+                camera.annotate_text_size = int(20 * scale)
+                camera.capture(stream,format="png")
+
+                frame = stream.getvalue()
+
+        return (b'--frame\r\n'
+                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+if __name__ == '__main__':
+	capture_picture('foo.jpg',0.5)
+
+
+
+
---
+++ run.py
@@ -0,0 +1,92 @@
+import io
+import os
+import RPi.GPIO as GPIO
+import time
+import json
+from flask import Flask, request, session, g, redirect, url_for, abort, \
+     render_template, flash, Response
+import picamera
+
+from  flask_restful import Resource, Api
+from camerafun import capture_picture, gen_videos
+# create our little application :)
+app = Flask(__name__)
+
+jsfile = "/home/pi/Documents/python/homecamera/camstate.json"
+
+def read_state():
+        with open(jsfile, 'r') as f:
+                return json.load(f)
+
+def write_state(state):
+        with open(jsfile, 'w') as f:
+                json.dump(state,f)
+                
+class CamInfo(Resource):
+
+        def __init__ (self):
+                filename = "/home/pi/Documents/python/homecamera/features.json"
+                with open(filename,'r') as f:
+                        self.info = json.load(f)
+                print(self.info)
+
+        def __str__(self):
+                return self.info
+        def get(self):
+                return self.info
+
+
+@app.route("/",methods=["GET","POST"])
+def home():
+   print("home")
+   try:
+       return render_template("/camera.html")
+   except Exception as ex:
+       print(ex)
+
+@app.route("/change_mode",methods=["GET","POST"])
+def change_mode():
+   mode = request.form["mode"]
+   return render_template("/camera.html",mode=mode)
+  
+@app.route('/take_picture')
+def take_picture():
+   caminfo = CamInfo()
+   print("taking a picture")
+   print(caminfo.info["name"])
+   return Response(capture_picture(caminfo.info["name"],1),
+   mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@app.route('/timelapse')
+def timelapse():
+   #caminfo = CamInfo()
+   print("timelapse")
+   try:
+        return Response(gen_videos(caminfo.info["name"],0.5),
+        mimetype='multipart/x-mixed-replace; boundary=frame')
+   except Exception as ex:
+        if state["Mode"].lower() == 'dashcam':
+                write_state({"Mode":"off","current_file":"None"})
+        print(ex)
+        return "error "
+
+led = 13
+led_onof = lambda x: GPIO.output(led,x)
+
+def setup_GPIO():
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setup(led,GPIO.OUT)
+
+try:
+        print("starting")
+        setup_GPIO()
+        api = Api(app)
+        caminfo = CamInfo()
+        api.add_resource(CamInfo,'/info')
+        print("added resource to rest")
+        led_onof(1)
+        app.run("0.0.0.0",port=19744)
+        led_onof(0)
+except Exception as ex:
+        led_onof(0)
+        print(ex)
